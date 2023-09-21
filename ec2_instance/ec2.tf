12a6a83408c9097bf422de527fb06888b3c00f51
Iamneo <techadmin@iamneo.ai>
2023-09-21 07:08:00 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -10,3 +10,8 @@ provider "aws" {
     access_key = "" 
     secret_key = ""
 } 
+resource "aws_instance" "linux_server"{
+    ami="SUSE Linux Enterprise Server 12 SP5" 
+    instance_type = "t2.micro" 
+}
+
